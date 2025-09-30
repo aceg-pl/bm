8af9703c7c48be920c30c1b79c9078edeb64c1e9
aceg-pl <adam0ceg@gmail.com>
2025-09-30 02:34:31 +0200

    Create myBookmarklet.js

---
+++ myBookmarklet.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name     My Bookmarklet Script
+// @version  1.0
+// @description Example external JS for bookmarklet
+// ==/UserScript==
+
+(function(){
+    // All your original bookmarklet code goes here
+    alert('Hello from external JS!');
+    
+    // Example: change page background color
+    document.body.style.backgroundColor = '#fffae6';
+})();
